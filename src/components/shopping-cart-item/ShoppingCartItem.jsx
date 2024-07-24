2c27b751cda12d8ed99318fc1051c6d9d6977706
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-07-24 12:27:23 +0200

    added thumbnails in order confirmation modal

--- src/components/shopping-cart-item/ShoppingCartItem.jsx
+++ src/components/shopping-cart-item/ShoppingCartItem.jsx
@@ -1,11 +1,16 @@
 import ShoppingCartItemSummary from '../shopping-cart-item-summary/ShoppingCartItemSummary'
 import RemoveItemButton from '../remove-item-button/RemoveItemButton'
 import convertToCurrency from '../../helpers/convertToCurrency'
+import getThumbnailByName from '../../helpers/getThumbnailByName'
+import data from '../../../data.json'
 import './shopping-cart-item.css'
 
 export default function ShoppingCartItem({ context, item, amount, singleItemPrice, totalItemPrice }) {
     return (
         <article className='shopping-cart-item'>
+            {
+                context === 'order-confirmation' && <img src={getThumbnailByName(data, item)} alt={item} />
+            }
             <ShoppingCartItemSummary
                 context={context}
                 item={item}
---
+++ src/helpers/getThumbnailByName.js
@@ -0,0 +1,8 @@
+export default function getThumbnailByName(items, name) {
+    for (let item of items) {
+        if (item.name === name) {
+            return `./src${(item.image.thumbnail).substring(1)}`
+        }
+    }
+    return null
+}
