55f0fc080e64ed09896a9ac01ba3ea24b9e8d5a8
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-07-25 16:36:39 +0200

    changed image paths

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -9,10 +9,7 @@ import './product-card.css'
 
 export default function ProductCard({ product }) {
     const { category, name, price, image } = product
-    const desktopImagePath = `./src/${image.desktop}`
-    const tabletImagePath = `./src/${image.tablet}`
-    const mobileImagePath = `./src/${image.mobile}`
-
+    
     return (
         <article className='product-card'>
             <header>
@@ -21,15 +18,15 @@ export default function ProductCard({ product }) {
                     <picture>
                         <source
                             media='(min-width: 1440px )'
-                            srcSet={desktopImagePath}
+                            srcSet={`src/${image.desktop}`}
                         />
                         <source
                             media='(min-width: 768px)'
-                            srcSet={tabletImagePath}
+                            srcSet={`src/${image.tablet}`}
                         />
                         <img
                             className='product-image'
-                            src={mobileImagePath}
+                            src={`src/${image.mobile}`}
                             alt={name}
                         />
                     </picture>
