ad1c8888579db1e38620bdf0ec3d73aef6cdcd35
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-07-25 16:44:14 +0200

    test different image paths

--- src/components/product-card/ProductCard.jsx
+++ src/components/product-card/ProductCard.jsx
@@ -18,15 +18,15 @@ export default function ProductCard({ product }) {
                     <picture>
                         <source
                             media='(min-width: 1440px )'
-                            srcSet={`src/${image.desktop}`}
+                            srcSet={`${image.desktop}`}
                         />
                         <source
                             media='(min-width: 768px)'
-                            srcSet={`src/${image.tablet}`}
+                            srcSet={`${image.tablet}`}
                         />
                         <img
                             className='product-image'
-                            src={`src/${image.mobile}`}
+                            src={`${image.mobile}`}
                             alt={name}
                         />
                     </picture>
