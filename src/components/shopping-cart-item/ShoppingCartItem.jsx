19618cfcaba2f04a73696db8c2b97da7b33b6f16
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-07-25 09:30:49 +0200

    moved all parts of shopping cart items into ShoppingCartItemComponent

--- src/components/shopping-cart-item/ShoppingCartItem.jsx
+++ src/components/shopping-cart-item/ShoppingCartItem.jsx
@@ -1,27 +1,87 @@
-import ShoppingCartItemSummary from '../shopping-cart-item-summary/ShoppingCartItemSummary'
-import RemoveItemButton from '../remove-item-button/RemoveItemButton'
+import { useContext } from 'react'
+
+// Context
+import { ShoppingCartContext } from '../../context/ShoppingCartContext'
+import { AddToCartButtonContext } from '../../context/AddToCartButtonContext'
+
+// Helpers
 import convertToCurrency from '../../helpers/convertToCurrency'
 import getThumbnailByName from '../../helpers/getThumbnailByName'
+
+// Constants
 import data from '../../../data.json'
+
+// Styles
 import './shopping-cart-item.css'
 
 export default function ShoppingCartItem({ context, item, amount, singleItemPrice, totalItemPrice }) {
+    const { updateShoppingCart } = useContext(ShoppingCartContext)
+    const { updateIsActive } = useContext(AddToCartButtonContext)
+
+    // Handle remove item from shopping cart button click
+    function handleClick() {
+        updateShoppingCart(item, 0)
+        updateIsActive(item, false)
+    }
+
     return (
         <article className='shopping-cart-item'>
+            {/* Item thumbnail */}
             {
-                context === 'order-confirmation' && <img src={getThumbnailByName(data, item)} alt={item} />
+                context === 'order-confirmation' && 
+                    <img 
+                        src={getThumbnailByName(data, item)} 
+                        alt={item} 
+                    />
             }
-            <ShoppingCartItemSummary
-                context={context}
-                item={item}
-                amount={amount}
-                singleItemPrice={singleItemPrice}
-                totalItemPrice={totalItemPrice}
-            />
+            {/* Item summary */}
+            <div className='item-summary'>
+                {/* Product title */}
+                <header>
+                    <h3 className='heading-4'>
+                        {item}
+                    </h3>
+                </header>
+                {/* Price calculation */}
+                <div className='price-calculation'>
+                    {/* Amount of items */}
+                    <p className='paragraph-5'>
+                        {`${amount}x`}
+                    </p>
+                    {/* Price per unit */}
+                    <p className='paragraph-1'>
+                        {convertToCurrency(singleItemPrice)}
+                    </p>
+                    {/* Total price for this item (in shopping cart) */}
+                    {
+                        context === 'shopping-cart' ?
+                            <p className='paragraph-3'>
+                                {convertToCurrency(totalItemPrice)}
+                            </p>
+                            :
+                            null
+                    }
+                </div>
+            </div>
             {
                 context === 'shopping-cart' ?
-                    <RemoveItemButton item={item} />
+                    // Remove item from shopping cart button
+                    <button
+                        className='remove-item-button'
+                        onClick={handleClick}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="10"
+                            height="10"
+                            fill="none"
+                            viewBox="0 0 10 10"
+                        >
+                            <path d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z" />
+                        </svg>
+                    </button>
                     :
+                    // Total price for this item (in order confirmation modal)
                     <p className='paragraph-4'>
                         {convertToCurrency(totalItemPrice)}
                     </p>
