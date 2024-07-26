36acab2a86a8fa14a51dbbd30e24635320118c2e
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-07-26 18:11:38 +0200

    changed image paths

--- src/helpers/getThumbnailByName.js
+++ src/helpers/getThumbnailByName.js
@@ -1,7 +1,7 @@
 export default function getThumbnailByName(items, name) {
     for (let item of items) {
         if (item.name === name) {
-            return `./src${(item.image.thumbnail).substring(1)}`
+            return `.${(item.image.thumbnail).substring(1)}`
         }
     }
     return null
